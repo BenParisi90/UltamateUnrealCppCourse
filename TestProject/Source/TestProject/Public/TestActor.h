4441c2b76d298417b57d1d0821359137d354128d
BenParisi <BenParisi90@gmail.com>
2024-01-06 01:02:39 -0800

    macros for exposing variable to blueprints

--- TestProject/Source/TestProject/Public/TestActor.h
+++ TestProject/Source/TestProject/Public/TestActor.h
@@ -18,9 +18,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:	
+private:
+	UPROPERTY(VisibleAnywhere)
 	float RunningTime;
+
+	UPROPERTY(EditAnywhere)
 	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere)
 	float TimeConstant = 5.f;
 
 };
