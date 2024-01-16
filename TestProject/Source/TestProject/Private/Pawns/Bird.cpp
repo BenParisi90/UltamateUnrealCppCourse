5a7c46ced5e22819c9ec2f90a3cce9c1a9fc0e06
Ben Parisi <BenParisi90@gmail.com>
2024-01-15 17:06:33 -0800

    add default input binding where "w" outputs 1 to the output window

--- TestProject/Source/TestProject/Private/Pawns/Bird.cpp
+++ TestProject/Source/TestProject/Private/Pawns/Bird.cpp
@@ -18,12 +18,18 @@ ABird::ABird()
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
 
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ABird::MoveForward(float Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), Value);
 }
 
 void ABird::Tick(float DeltaTime)
@@ -37,5 +43,6 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ABird::MoveForward);
 }
 
